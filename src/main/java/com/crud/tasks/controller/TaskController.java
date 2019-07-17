2d2aab8e44df4ad384dd9a4311cea80d736aee9e
ewaradomska <ewa.radomska@gmail.com>
2019-07-17 11:23:25 +0200

    db connect

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,10 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +15,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+    @Autowired
+    TaskMapper taskMapper;
+    @Autowired
+    DbService dbService;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks(){
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(dbService.getAllTasks());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long id){
-        return new TaskDto(1L, "test_title", "test_content");
+        return taskMapper.mapToTaskDto(dbService.getTask(id));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
